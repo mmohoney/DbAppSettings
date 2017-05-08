110389343a495ce905fbebf4c4eab287dfda76ea
Matthew Mohoney <mattmohoney21@gmail.com>
2017-05-07 20:33:14 -0500

    unit tested each type and custom obects

--- DbAppSettings/Source/DbAppSettings.Test/Model/Domain/DbAppSettingTypeTest.cs
+++ DbAppSettings/Source/DbAppSettings.Test/Model/Domain/DbAppSettingTypeTest.cs
@@ -85,10 +85,181 @@ namespace DbAppSettings.Test.Model.Domain
             Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
         }
 
+        public class decimalSetting : DbAppSetting<decimalSetting, decimal> { public override decimal InitialValue => 1.25M; }
+        [Test]
+        public void DbAppSetting_decimal()
+        {
+            decimalSetting setting = new decimalSetting();
 
+            Assert.IsTrue(setting.InitialValue == 1.25M);
+            Assert.IsTrue(setting.InternalValue == 1.25M);
 
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 5.10M.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
 
+            Assert.IsTrue(setting.InitialValue == 1.25M);
+            Assert.IsTrue(setting.InternalValue == 5.10M);
 
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class doubleSetting : DbAppSetting<doubleSetting, double> { public override double InitialValue => 1.25; }
+        [Test]
+        public void DbAppSetting_double()
+        {
+            doubleSetting setting = new doubleSetting();
+
+            Assert.IsTrue(setting.InitialValue == 1.25);
+            Assert.IsTrue(setting.InternalValue == 1.25);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 5.10.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 1.25);
+            Assert.IsTrue(setting.InternalValue == 5.10);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class floatSetting : DbAppSetting<floatSetting, float> { public override float InitialValue => 1.25F; }
+        [Test]
+        public void DbAppSetting_float()
+        {
+            floatSetting setting = new floatSetting();
+
+            Assert.IsTrue(setting.InitialValue == 1.25F);
+            Assert.IsTrue(setting.InternalValue == 1.25F);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 5.10F.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 1.25F);
+            Assert.IsTrue(setting.InternalValue == 5.10F);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class intSetting : DbAppSetting<intSetting, int> { public override int InitialValue => 1; }
+        [Test]
+        public void DbAppSetting_int()
+        {
+            intSetting setting = new intSetting();
+
+            Assert.IsTrue(setting.InitialValue == 1);
+            Assert.IsTrue(setting.InternalValue == 1);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 5.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 1);
+            Assert.IsTrue(setting.InternalValue == 5);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class longSetting : DbAppSetting<longSetting, long> { public override long InitialValue => 2147483648; }
+        [Test]
+        public void DbAppSetting_long()
+        {
+            longSetting setting = new longSetting();
+
+            Assert.IsTrue(setting.InitialValue == 2147483648);
+            Assert.IsTrue(setting.InternalValue == 2147483648);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 2147483649.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 2147483648);
+            Assert.IsTrue(setting.InternalValue == 2147483649);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class sbyteSetting : DbAppSetting<sbyteSetting, sbyte> { public override sbyte InitialValue => -127; }
+        [Test]
+        public void DbAppSetting_sbyte()
+        {
+            sbyteSetting setting = new sbyteSetting();
+
+            Assert.IsTrue(setting.InitialValue == -127);
+            Assert.IsTrue(setting.InternalValue == -127);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 127.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == -127);
+            Assert.IsTrue(setting.InternalValue == 127);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class shortSetting : DbAppSetting<shortSetting, short> { public override short InitialValue => 1; }
+        [Test]
+        public void DbAppSetting_short()
+        {
+            shortSetting setting = new shortSetting();
+
+            Assert.IsTrue(setting.InitialValue == 1);
+            Assert.IsTrue(setting.InternalValue == 1);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 2.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 1);
+            Assert.IsTrue(setting.InternalValue == 2);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class stringSetting : DbAppSetting<stringSetting, string> { public override string InitialValue => "test"; }
+        [Test]
+        public void DbAppSetting_string()
+        {
+            stringSetting setting = new stringSetting();
+
+            Assert.IsTrue(setting.InitialValue == "test");
+            Assert.IsTrue(setting.InternalValue == "test");
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = "new value".ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == "test");
+            Assert.IsTrue(setting.InternalValue == "new value");
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
 
         public class StringCollectionSetting : DbAppSetting<StringCollectionSetting, StringCollection> { public override StringCollection InitialValue => new StringCollection { "One", "Two", "Three" }; }
         [Test]
@@ -113,6 +284,138 @@ namespace DbAppSettings.Test.Model.Domain
             Assert.IsTrue(initialStringList.SequenceEqual(initialValueList));
             internalValueList = setting.InternalValue.Cast<string>().ToList();
             Assert.IsTrue(newStringList.SequenceEqual(internalValueList));
+        }
+
+        public class DateTimeSetting : DbAppSetting<DateTimeSetting, DateTime> { public override DateTime InitialValue => DateTime.Today; }
+        [Test]
+        public void DbAppSetting_DateTime()
+        {
+            DateTimeSetting setting = new DateTimeSetting();
+
+            Assert.IsTrue(setting.InitialValue == DateTime.Today);
+            Assert.IsTrue(setting.InternalValue == DateTime.Today);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = DateTime.Today.AddDays(1).ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == DateTime.Today);
+            Assert.IsTrue(setting.InternalValue == DateTime.Today.AddDays(1));
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class GuidSetting : DbAppSetting<GuidSetting, Guid> { public override Guid InitialValue => new Guid("9245fe4a-d402-451c-b9ed-9c1a04247482"); }
+        [Test]
+        public void DbAppSetting_Guid()
+        {
+            GuidSetting setting = new GuidSetting();
+
+            Assert.IsTrue(setting.InitialValue == new Guid("9245fe4a-d402-451c-b9ed-9c1a04247482"));
+            Assert.IsTrue(setting.InternalValue == new Guid("9245fe4a-d402-451c-b9ed-9c1a04247482"));
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = new Guid("1245fe4a-d402-451c-b9ed-9c1a04247482").ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == new Guid("9245fe4a-d402-451c-b9ed-9c1a04247482"));
+            Assert.IsTrue(setting.InternalValue == new Guid("1245fe4a-d402-451c-b9ed-9c1a04247482"));
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class TimeSpanSetting : DbAppSetting<TimeSpanSetting, TimeSpan> { public override TimeSpan InitialValue => new TimeSpan(1, 1, 1, 1); }
+        [Test]
+        public void DbAppSetting_TimeSpan()
+        {
+            TimeSpanSetting setting = new TimeSpanSetting();
+
+            Assert.IsTrue(setting.InitialValue == new TimeSpan(1, 1, 1, 1));
+            Assert.IsTrue(setting.InternalValue == new TimeSpan(1, 1, 1, 1));
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = new TimeSpan(2, 1, 1, 1).ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == new TimeSpan(1, 1, 1, 1));
+            Assert.IsTrue(setting.InternalValue == new TimeSpan(2, 1, 1, 1));
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class uintSetting : DbAppSetting<uintSetting, uint> { public override uint InitialValue => 4294967295; }
+        [Test]
+        public void DbAppSetting_uint()
+        {
+            uintSetting setting = new uintSetting();
+
+            Assert.IsTrue(setting.InitialValue == 4294967295);
+            Assert.IsTrue(setting.InternalValue == 4294967295);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 4294967294.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 4294967295);
+            Assert.IsTrue(setting.InternalValue == 4294967294);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class ulongSetting : DbAppSetting<ulongSetting, ulong> { public override ulong InitialValue => 18446744073709551615; }
+        [Test]
+        public void DbAppSetting_ulong()
+        {
+            ulongSetting setting = new ulongSetting();
+
+            Assert.IsTrue(setting.InitialValue == 18446744073709551615);
+            Assert.IsTrue(setting.InternalValue == 18446744073709551615);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 18446744073709551614.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 18446744073709551615);
+            Assert.IsTrue(setting.InternalValue == 18446744073709551614);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class ushortSetting : DbAppSetting<ushortSetting, ushort> { public override ushort InitialValue => 65535; }
+        [Test]
+        public void DbAppSetting_ushort()
+        {
+            ushortSetting setting = new ushortSetting();
+
+            Assert.IsTrue(setting.InitialValue == 65535);
+            Assert.IsTrue(setting.InternalValue == 65535);
+
+            DbAppSettingDto settingDto = new DbAppSettingDto() { Key = setting.FullSettingName, Value = 65534.ToString(), Type = setting.TypeString };
+            setting.From(settingDto);
+
+            Assert.IsTrue(setting.InitialValue == 65535);
+            Assert.IsTrue(setting.InternalValue == 65534);
+
+            DbAppSettingDto toDto = setting.ToDto();
+            Assert.IsTrue(toDto.ApplicationKey == settingDto.ApplicationKey);
+            Assert.IsTrue(toDto.Key == settingDto.Key);
+            Assert.IsTrue(toDto.Type == settingDto.Type);
+            Assert.IsTrue(toDto.Value.Equals(settingDto.Value, StringComparison.InvariantCultureIgnoreCase));
         }
 
         public class MyTestClassSetting : DbAppSetting<MyTestClassSetting, MyTestClass> { public override MyTestClass InitialValue => new MyTestClass(); }
