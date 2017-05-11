389f2068d3e3538d1aa900db583dcd72eecf2a5e
Matt <mattmohoney21@gmail.com>
2017-05-10 22:58:50 -0500

    - added external reference and included it in the folder for Web.Entity

--- DbAppSettings/Source/DbAppSettings.Test/Properties/DbAppSettingsTestSettings.cs
+++ DbAppSettings/Source/DbAppSettings.Test/Properties/DbAppSettingsTestSettings.cs
@@ -14,3 +14,26 @@ namespace DbAppSettings.Test.Properties
         public class MyCustomObjectSetting : DbAppSetting<MyCustomObjectSetting, MySettingsClass> { public override MySettingsClass InitialValue => new MySettingsClass(); }
     }
 }
+
+namespace DbAppSettings.MyAssembly
+{
+    public class MyAssemblySettings
+    {
+        public class EnableLogging : DbAppSetting<EnableLogging, bool> { public override bool InitialValue => false; }
+        public class MyOtherSetting : DbAppSetting<MyOtherSetting, SettingsObject> { public override SettingsObject InitialValue => new SettingsObject(); }
+    }
+
+    public class SettingsObject
+    {
+        public int PropertyOne => 1;
+        public string PropertyTwo => "String settings";
+
+        public void DoSomeWork()
+        {
+            if (MyAssemblySettings.MyOtherSetting.Value.PropertyOne == 1)
+            {
+                //Do other work
+            }
+        }
+    }
+}
